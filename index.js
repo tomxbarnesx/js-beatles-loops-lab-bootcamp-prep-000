1c75bb2a6c17b693293802bf653af679f5bffc1b
Thomas Barnes <barnes.thomas.c@gmail.com>
2018-05-22 22:36:33 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,23 @@
+function theBeatlesPlay(musicians, instruments) {
+  var array = [];
+  for (i = 0; i < musicians.length; i++){
+    array.push(musicians[i] + " plays " + instruments[i]);
+  }
+  return array;
+}
+
+function johnLennonFacts(facts){
+ while (i < facts.length) {
+   facts[i] + "!!!";
+ }
+ return facts;
+}
+
+function iLoveTheBeatles(n){
+  var emptyArray = [];
+  do {
+    emptyArray.push("I love the Beatles!");
+  } while (n < 15);
+  return emptyArray;
+}
+
